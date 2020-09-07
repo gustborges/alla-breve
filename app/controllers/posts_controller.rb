5cff7fd1e51629a6c34bf274d5f9a51919bda92a
Gustavo Borges <gustborges@gmail.com>
2020-09-07 01:21:25 -0300

    Fixed ckeditor with image uploads

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :post_find, only: [:show, :edit, :update, :destroy]
 
   def index
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: {maximum: 140}
+  validates :subtitle, presence: true, length: {maximum: 200}
+  validates :text, presence: true
 end
