dc94af079c2d7a4fe2f8db422fe93c00ad42323f
Gustavo Borges <gustborges@gmail.com>
2020-09-13 23:42:41 -0300

    Added thumbnails to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :post_find, only: [:show, :edit, :update, :destroy]
+  before_action :post_find, only: [:show, :edit, :destroy]
 
   def index
     @posts = Post.all
@@ -27,11 +27,10 @@ class PostsController < ApplicationController
   def edit; end
 
   def update
+    @post = Post.find(params[:id])
     if @post.update(post_params)
-      flash[:notice] = "Successfully updated post!"
       redirect_to post_path(@post)
     else
-      flash[:alert] = "Error updating post!"
       render :edit
     end
   end
