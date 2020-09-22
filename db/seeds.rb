f1a60f4029955fa23b18da707224094ad02f4b13
Gustavo Borges <gustborges@gmail.com>
2020-09-22 18:32:41 -0300

    Added admin to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+maxlima = User.create(
+  email: "maxlimascm@gmail.com",
+  password: "raguzinho201",
+  admin: true
+)
